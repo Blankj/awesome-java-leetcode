a4579350fce40a40e320c162fe131baedf6bda3d
Blankj <625783482@qq.com>
2017-10-18 15:21:21 +0800

    feat: add 010

--- src/com/blankj/hard/_010/Solution.java
+++ src/com/blankj/hard/_010/Solution.java
@@ -10,53 +10,56 @@ package com.blankj.hard._010;
  */
 public class Solution {
 //    public boolean isMatch(String s, String p) {
-//        if (p.length() == 0) return s.length() == 0;
+//        if (p.isEmpty()) return s.isEmpty();
 //        if (p.length() == 1) {
-//            if (s.length() < 1) return false;
-//            if (p.charAt(0) != s.charAt(0) && p.charAt(0) != '.') return false;
-//            return isMatch(s.substring(1), p.substring(1));
+//            return s.length() == 1 && (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.');
 //        }
 //        if (p.charAt(1) != '*') {
-//            if (s.length() < 1) return false;
-//            if (p.charAt(0) != s.charAt(0) && p.charAt(0) != '.') return false;
-//            return isMatch(s.substring(1), p.substring(1));
-//        } else {
-//            // match 0 preceding element
-//            if (isMatch(s, p.substring(2))) return true;
-//            int i = 0;
-//            // match 1 or more preceding element
-//            while (i < s.length() && (p.charAt(0) == s.charAt(i) || p.charAt(0) == '.')) {
-//                if (isMatch(s.substring(i + 1), p.substring(2))) {
-//                    return true;
-//                }
-//                ++i;
-//            }
-//            return false;
+//            if (s.isEmpty()) return false;
+//            return (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.')
+//                    && isMatch(s.substring(1), p.substring(1));
 //        }
+//        // match 1 or more preceding element
+//        while (!s.isEmpty() && (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.')) {
+//            if (isMatch(s, p.substring(2))) return true;
+//            s = s.substring(1);
+//        }
+//        // match 0 preceding element
+//        return isMatch(s, p.substring(2));
+//    }
+//
+//    public boolean isMatch(String s, String p) {
+//        if (p.isEmpty()) return s.isEmpty();
+//        if (p.length() > 1 && p.charAt(1) == '*') {
+//            return isMatch(s, p.substring(2))
+//                    || (!s.isEmpty() && (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.')
+//                    && isMatch(s.substring(1), p));
+//        }
+//        return !s.isEmpty() && (p.charAt(0) == s.charAt(0) || p.charAt(0) == '.')
+//                && isMatch(s.substring(1), p.substring(1));
 //    }
 
     public boolean isMatch(String s, String p) {
         if (p.length() == 0) return s.length() == 0;
         int sL = s.length(), pL = p.length();
         boolean[][] dp = new boolean[sL + 1][pL + 1];
+        char[] sc = s.toCharArray(), pc = p.toCharArray();
         dp[0][0] = true;
-        for (int i = 1; i < pL; ++i) {
-            if (p.charAt(i) == '*' && dp[0][i - 1]) {
-                dp[0][i + 1] = true;
+        for (int i = 2; i <= pL; ++i) {
+            if (pc[i - 1] == '*' && dp[0][i - 2]) {
+                dp[0][i] = true;
             }
         }
-        for (int i = 0; i < sL; ++i) {
-            for (int j = 0; j < pL; ++j) {
-                char c1 = s.charAt(i), c2 = p.charAt(j);
-                if (c2 == '.' || c2 == c1) {
-                    dp[i + 1][j + 1] = dp[i][j];
+        for (int i = 1; i <= sL; ++i) {
+            for (int j = 1; j <= pL; ++j) {
+                if (pc[j - 1] == '.' || pc[j - 1] == sc[i - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
                 }
-                if (c2 == '*') {
-                    c2 = p.charAt(j - 1);
-                    if (c2 == c1 || c2 == '.') {
-                        dp[i + 1][j + 1] = dp[i + 1][j] || dp[i][j + 1] || dp[i + 1][j - 1];
+                if (pc[j - 1] == '*') {
+                    if (pc[j - 2] == sc[i - 1] || pc[j - 2] == '.') {
+                        dp[i][j] = dp[i - 1][j] || dp[i][j - 2];
                     } else {
-                        dp[i + 1][j + 1] = dp[i + 1][j - 1];
+                        dp[i][j] = dp[i][j - 2];
                     }
                 }
             }

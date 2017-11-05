99608a03b17b539f927f7d2ad9738a7fcc21e5e8
Blankj <625783482@qq.com>
2017-11-05 16:20:55 +0800

    feat: update 005

--- src/com/blankj/medium/_005/Solution.java
+++ src/com/blankj/medium/_005/Solution.java
@@ -9,28 +9,53 @@ package com.blankj.medium._005;
  * </pre>
  */
 public class Solution {
-    int st, end;
+//    int st, end;
+//
+//    public String longestPalindrome(String s) {
+////        st = 0;
+////        end = 0;
+//        int len = s.length();
+//        if (len <= 1) return s;
+//        char[] chars = s.toCharArray();
+//        for (int i = 0; i < len; i++) {
+//            helper(chars, i, i);
+//            helper(chars, i, i + 1);
+//        }
+//        return s.substring(st, end + 1);
+//    }
+//
+//    private void helper(char[] chars, int l, int r) {
+//        while (l >= 0 && r < chars.length && chars[l] == chars[r]) {
+//            --l;
+//            ++r;
+//        }
+//        if (end - st < r - l - 2) {
+//            st = l + 1;
+//            end = r - 1;
+//        }
+//    }
 
     public String longestPalindrome(String s) {
         int len = s.length();
         if (len <= 1) return s;
+        int st = 0, end = 0;
         char[] chars = s.toCharArray();
+        boolean[][] dp = new boolean[len][len];
         for (int i = 0; i < len; i++) {
-            helper(chars, i, i);
-            helper(chars, i, i + 1);
+            dp[i][i] = true;
+            for (int j = 0; j < i; j++) {
+                if (j + 1 == i) {
+                    dp[j][i] = chars[j] == chars[i];
+                } else {
+                    dp[j][i] = dp[j + 1][i - 1] && chars[j] == chars[i];
+                }
+                if (dp[j][i] && i - j > end - st) {
+                    st = j;
+                    end = i;
+                }
+            }
         }
         return s.substring(st, end + 1);
-    }
-
-    private void helper(char[] chars, int l, int r) {
-        while (l >= 0 && r < chars.length && chars[l] == chars[r]) {
-            --l;
-            ++r;
-        }
-        if (end - st < r - l - 2) {
-            st = l + 1;
-            end = r - 1;
-        }
     }
 
     public static void main(String[] args) {

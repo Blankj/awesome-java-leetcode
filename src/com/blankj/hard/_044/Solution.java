58ee959c4fdffdc5ea7c215f44afc52ec7a5db49
Blankj <625783482@qq.com>
2017-11-06 23:06:27 +0800

    feat: update 005

--- src/com/blankj/hard/_044/Solution.java
+++ src/com/blankj/hard/_044/Solution.java
@@ -1,5 +1,9 @@
 package com.blankj.hard._044;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * <pre>
  *     author: Blankj
@@ -30,35 +34,74 @@ public class Solution {
 //        return pi == pl;
 //    }
 
-    public boolean isMatch(String s, String p) {
-        if (p.length() == 0) return s.length() == 0;
-        int sl = s.length(), pl = p.length();
-        boolean[][] dp = new boolean[sl + 1][pl + 1];
-        char[] sc = s.toCharArray(), pc = p.toCharArray();
-        dp[0][0] = true;
-        for (int i = 1; i <= pl; ++i) {
-            if (pc[i - 1] == '*') dp[0][i] = dp[0][i - 1];
+//    public boolean isMatch(String s, String p) {
+//        if (p.length() == 0) return s.length() == 0;
+//        int sl = s.length(), pl = p.length();
+//        boolean[][] dp = new boolean[sl + 1][pl + 1];
+//        char[] sc = s.toCharArray(), pc = p.toCharArray();
+//        dp[0][0] = true;
+//        for (int i = 1; i <= pl; ++i) {
+//            if (pc[i - 1] == '*') dp[0][i] = dp[0][i - 1];
+//        }
+//        for (int i = 1; i <= sl; ++i) {
+//            for (int j = 1; j <= pl; ++j) {
+//                if (pc[j - 1] != '*') {
+//                    dp[i][j] = dp[i - 1][j - 1] && (sc[i - 1] == pc[j - 1] || pc[j - 1] == '?');
+//                } else {
+//                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+//                }
+//            }
+//        }
+//        return dp[sl][pl];
+//    }
+
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        int len = words.length;
+        if (len == 0) return Collections.emptyList();
+        List<String> ans = new ArrayList<>();
+        StringBuilder spaces = new StringBuilder();
+        for (int i = 0; i < maxWidth; ++i) {
+            spaces.append(" ");
         }
-        for (int i = 1; i <= sl; ++i) {
-            for (int j = 1; j <= pl; ++j) {
-                if (pc[j - 1] != '*') {
-                    dp[i][j] = dp[i - 1][j - 1] && (sc[i - 1] == pc[j - 1] || pc[j - 1] == '?');
+        int sLen = -1, left = 0;
+        for (int i = 0; i < len; ++i) {
+            if (sLen + words[i].length() + 1 <= maxWidth) {
+                sLen += words[i].length() + 1;
+            } else {
+                StringBuilder sub = new StringBuilder(words[left]);
+                int rest = maxWidth - sLen;
+                int seg = i - left;
+                if (seg == 0) {
+                    sub.append(spaces.substring(0, rest));
                 } else {
-                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+                    int leastSpace = rest / seg + 1;
+                    int restSpace = rest % seg;
+                    for (int j = left + 1; j < i; ++j) {
+                        if (restSpace-- > 0) {
+                            sub.append(spaces.substring(0, leastSpace + 1)).append(words[j]);
+                        } else {
+                            sub.append(spaces.substring(0, leastSpace)).append(words[j]);
+                        }
+                    }
                 }
+                ans.add(sub.toString());
+                left = i;
+                sLen = words[i].length();
             }
         }
-        return dp[sl][pl];
+        StringBuilder sub = new StringBuilder(words[left]);
+        for (int i = left + 1; i < len; ++i) {
+            sub.append(" ").append(words[i]);
+        }
+        ans.add(sub + spaces.substring(0, maxWidth - sub.length()));
+        return ans;
     }
+
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.isMatch("aa", "a"));     // false
-        System.out.println(solution.isMatch("aa", "aa"));    // true
-        System.out.println(solution.isMatch("aaa", "aa"));   // false
-        System.out.println(solution.isMatch("aa", "*"));     // true
-        System.out.println(solution.isMatch("aa", "a*"));    // true
-        System.out.println(solution.isMatch("ab", "?*"));    // true
-        System.out.println(solution.isMatch("aab", "c*a*b"));// false
+        System.out.println(solution.fullJustify(new String[]{"", ""}, 0));
+        System.out.println(solution.fullJustify(new String[]{"a"}, 1));
+        System.out.println(solution.fullJustify(new String[]{"This", "is", "an", "example", "of", "text", "justification."}, 16));
     }
 }

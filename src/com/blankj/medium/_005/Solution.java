5765dbc6301e30251449096e49efa9c3d17163cb
Blankj <625783482@qq.com>
2017-11-06 18:24:20 +0800

    feat: update 005

--- src/com/blankj/medium/_005/Solution.java
+++ src/com/blankj/medium/_005/Solution.java
@@ -35,32 +35,38 @@ public class Solution {
 //        }
 //    }
 
+//    public String longestPalindrome(String s) {
+//        int len = s.length();
+//        if (len <= 1) return s;
+//        int st = 0, end = 0;
+//        char[] chars = s.toCharArray();
+//        boolean[][] dp = new boolean[len][len];
+//        for (int i = 0; i < len; i++) {
+//            dp[i][i] = true;
+//            for (int j = 0; j < i; j++) {
+//                if (j + 1 == i) {
+//                    dp[j][i] = chars[j] == chars[i];
+//                } else {
+//                    dp[j][i] = dp[j + 1][i - 1] && chars[j] == chars[i];
+//                }
+//                if (dp[j][i] && i - j > end - st) {
+//                    st = j;
+//                    end = i;
+//                }
+//            }
+//        }
+//        return s.substring(st, end + 1);
+//    }
+
     public String longestPalindrome(String s) {
-        int len = s.length();
-        if (len <= 1) return s;
-        int st = 0, end = 0;
-        char[] chars = s.toCharArray();
-        boolean[][] dp = new boolean[len][len];
-        for (int i = 0; i < len; i++) {
-            dp[i][i] = true;
-            for (int j = 0; j < i; j++) {
-                if (j + 1 == i) {
-                    dp[j][i] = chars[j] == chars[i];
-                } else {
-                    dp[j][i] = dp[j + 1][i - 1] && chars[j] == chars[i];
-                }
-                if (dp[j][i] && i - j > end - st) {
-                    st = j;
-                    end = i;
-                }
-            }
-        }
-        return s.substring(st, end + 1);
+
+        return s;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
         System.out.println(solution.longestPalindrome("babad"));
         System.out.println(solution.longestPalindrome("cbbd"));
+
     }
 }
